13d43e3975fbccb5e0056149b6fc5695f70d791c
Daniel Santamaría <81576160+DanSantamaria@users.noreply.github.com>
2026-04-06 20:05:13 +0000

    merge con profile selection

---
+++ src/front/data/mockUsers.js
@@ -0,0 +1,20 @@
+export const mockUsers = [
+  {
+    id: 1,
+    username: "papa@example.com",
+    password: "123456",   // solo para pruebas
+    role: "parent",
+    profileName: "Papá",
+    avatar: "/assets/img/Cashtor.jpg",
+    pin: "1234"
+  },
+  {
+    id: 2,
+    username: "dani@example.com",
+    password: "abcdef",
+    role: "child",
+    profileName: "Dani",
+    avatar: "/assets/img/avatar2.png",
+    pin: "0000"
+  }
+];
